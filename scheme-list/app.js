b482b81475201273e5c10dfe2585169ba8589d99
Jayprecode <jayprecode@gmail.com>
2021-01-22 11:40:13 +0100

    initial commit

---
+++ scheme-list/app.js
@@ -0,0 +1,153 @@
+let form = document.querySelector("#s__form"),
+	taskList = document.querySelector(".s__collection"),
+	clearBtn = document.querySelector(".clear__scheme"),
+	filter = document.querySelector("#s__filter"),
+	taskInput = document.querySelector("#scheme");
+
+loadEventlisteners();
+//Load All event listeners
+function loadEventlisteners() {
+	//DOM Load event
+	document.addEventListener("DOMContentLoaded", getTasks);
+	//Add task events
+	form.addEventListener("submit", addTask);
+	//Remove task event
+	taskList.addEventListener("click", removeTask);
+	//Clear task event
+	clearBtn.addEventListener("click", clearTasks);
+	//Filter tasks event
+	filter.addEventListener("keyup", filterTasks);
+}
+
+//Get tasks from Local Storage
+function getTasks() {
+	let tasks;
+	if (localStorage.getItem("tasks") === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem("tasks"));
+	}
+
+	tasks.forEach(function (task) {
+		//Create li element
+		const li = document.createElement("li");
+		//Add class
+		li.className = "collection-item";
+		//Create text node and append to li
+		li.appendChild(document.createTextNode(task));
+		//Create new link element
+		const link = document.createElement("a");
+		//Add class
+		link.className = "delete-item secondary-content";
+		//Add icon html
+		link.innerHTML = "<i class='fa fa-remove'></i>";
+		//Append the link to li
+		li.appendChild(link);
+		//Append the li to ul
+		taskList.appendChild(li);
+	});
+}
+
+//Add task
+function addTask(e) {
+	if (taskInput.value === "") {
+		alert("Add a task");
+	}
+
+	//Create li element
+	const li = document.createElement("li");
+	//Add class
+	li.className = "collection-item";
+	//Create text node and append to li
+	li.appendChild(document.createTextNode(taskInput.value));
+	//Create new link element
+	const link = document.createElement("a");
+	//Add class
+	link.className = "delete-item secondary-content";
+	//Add icon html
+	link.innerHTML = "<i class='fa fa-remove'></i>";
+	//Append the link to li
+	li.appendChild(link);
+	//Append the li to ul
+	taskList.appendChild(li);
+
+	//Store in Local Storage
+	storeTaskInLocalStorage(taskInput.value);
+
+	//Clear input
+	taskInput.value = "";
+
+	//Prevents browser from refreshing
+	e.preventDefault();
+}
+
+//Store Task
+function storeTaskInLocalStorage(task) {
+	let tasks;
+	if (localStorage.getItem("tasks") === null) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem("tasks"));
+	}
+	tasks.push(task);
+
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//Remove task
+function removeTask(e) {
+	if (e.target.parentElement.classList.contains("delete-item")) {
+		if (confirm("Are you Sure?")) {
+			e.target.parentElement.parentElement.remove();
+			//Remove from Local Storage
+			removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+		}
+	}
+}
+
+//Remove from Local Storage
+function removeTaskFromLocalStorage(taskItem) {
+	let tasks;
+	if (localStorage.getItem("tasks") === null) {
+		task = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem("tasks"));
+	}
+	tasks.forEach(function (task, index) {
+		if (taskItem.textContent === task) {
+			tasks.splice(index, 1);
+		}
+	});
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//Clear Tasks
+function clearTasks() {
+	// taskList.innerHTML = "";
+	//or
+	while (taskList.firstChild) {
+		taskList.removeChild(taskList.firstChild);
+	}
+
+	//Clear from Local storage
+	clearTasksFromLocalStorage();
+}
+
+//Clear from Local storage
+
+function clearTasksFromLocalStorage() {
+	localStorage.clear();
+}
+
+//Filter Tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+	document.querySelectorAll(".collection-item").forEach(function (task) {
+		const item = task.firstChild.textContent;
+		if (item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = "block";
+		} else {
+			task.style.display = "none";
+		}
+	});
+}
